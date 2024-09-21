64c191b238056563d84cb95e5132403195f8ed68
cgardne89 <cgardne89@gmail.com>
2024-09-21 13:53:34 -0500

    added wrapping with modules and started symbol encryption

--- Shift Key encryption.py
+++ Shift Key encryption.py
@@ -1,12 +1,35 @@
 # Shift Key Encryption and Decryption
 
-
-
 Letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", 
         "J", "K", "L", "M", "N", "O", "P", "Q", "R",
         "S", "T", "U", "V", "W", "X", "Y", "Z"]
+symbols = ["!", "@", "#", "$", "%", "^", "&", "*", 
+           "(", ")", "-", "_", "=", "+", "[", "]", 
+           "{", "}", "|", "\\", ":", ";", "\"", "'", 
+           "<", ">", ",", ".", "?", "/"]
 
-    
+
+
+def check_encryptOrDecrypt(ciphered):
+    if (Encrypt_or_Decrypt) == "Decrypt":
+        ciphered = ciphered - shiftKey
+    else:
+        ciphered = ciphered + shiftKey
+
+    return ciphered
+
+def do_Letter_Wrapping(ciphered):
+    if (ciphered) >= 25 or (ciphered) <= 0:
+        ciphered = ciphered % 26
+    return ciphered
+
+def do_Number_Wrapping(ciphered):
+    if (ciphered) >= 0 and (ciphered) <= 9:
+        return ciphered
+    if (ciphered) > 9 or (ciphered) < 0:
+        ciphered = ciphered % 10
+        return ciphered
+
 while True:
     
     # User initial input
@@ -14,7 +37,6 @@ while True:
     Encrypt_or_Decrypt = Encrypt_or_Decrypt.capitalize()
 
     if (Encrypt_or_Decrypt) == "Decrypt" or (Encrypt_or_Decrypt) == "Encrypt":
-        
         
         phrase = input("Phrase: ")
         phrase = phrase.upper()
@@ -26,38 +48,27 @@ while True:
 
         # Cipher Logic
         for char in phrase:
-
-
             if char in Letters:
-
                 # Function this
                 charPos = Letters.index(char)
-                if (Encrypt_or_Decrypt) == "Decrypt":
-                    ciphered = int(charPos) - shiftKey
-                if (Encrypt_or_Decrypt) == "Encrypt":
-                    ciphered = int(charPos) + shiftKey
-                    
-                # Function this
-                getCiph = Letters[ciphered]
-                totalCiphered.append(getCiph)
+                ciphered = int(charPos)
+
+
+                # create wrapping
+                ciphered = check_encryptOrDecrypt(ciphered)
+                ciphered = Letters[do_Letter_Wrapping(ciphered)]
+
+                totalCiphered.append(ciphered)
 
             if (char.isnumeric()):
+                ciphered = int(char)
 
-                # Function this
-                if (Encrypt_or_Decrypt) == "Decrypt":
-                    number = int(char) - shiftKey
-                if (Encrypt_or_Decrypt) == "Encrypt":
-                    number = int(char) + shiftKey
+                # check for users input
+                ciphered = check_encryptOrDecrypt(ciphered)
 
-                # function this
-                if (number) > 0 and (number) < 10:
-                    totalCiphered.append(str(number))
-                if (number) == 0:
-                    number = number + 9
-                    totalCiphered.append(str(number))
-                elif (number) >= 10 or (number) <= -1:
-                    number = number % 9
-                    totalCiphered.append(str(number))
+                # wrapping numbers around 1 - 9
+                ciphered = do_Number_Wrapping(ciphered)
+                totalCiphered.append(str(ciphered))
 
 
         # Result
